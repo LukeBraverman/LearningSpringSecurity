c6eb83c9aaa17da4eb92604667b55a7fffb738d8
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-08 12:54:07 +0100

    Setting up git repo

---
+++ src/main/java/com/learningspringsecurity/demospringsecurity/DemospringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.learningspringsecurity.demospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringsecurityApplication.class, args);
+    }
+
+}
