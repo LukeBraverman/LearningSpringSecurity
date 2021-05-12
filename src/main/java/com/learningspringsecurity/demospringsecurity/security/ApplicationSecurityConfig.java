d4a73f78f93578a66792e63e20934aa4c4e5493a
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-12 22:23:10 +0100

    inProgress

--- src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
@@ -1,21 +1,71 @@
 package com.learningspringsecurity.demospringsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/","index","/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser,
+                tomUser);
+
     }
 }
---
+++ src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationUserRole.java
@@ -0,0 +1,26 @@
+package com.learningspringsecurity.demospringsecurity.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.learningspringsecurity.demospringsecurity.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ));
+
+
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
