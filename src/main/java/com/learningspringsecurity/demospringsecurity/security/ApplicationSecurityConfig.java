ac0cf23acdf5999b696922d6e972a82251e1ddb8
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-14 10:27:30 +0100

    finished csrf

--- src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.learningspringsecurity.demospringsecurity.security.ApplicationUserRole.*;
 
@@ -37,7 +38,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()//TODO:WILL BE TAUGHT
+                .csrf().disable()
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                //.and()
                 .authorizeRequests()
                 /*
                 .antMatchers("/","index","/css/*", "/js/*").permitAll()
