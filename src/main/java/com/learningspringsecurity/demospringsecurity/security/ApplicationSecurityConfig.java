97cb2b7d0343f8b5e9ec7a75f752026760ebea80
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-14 19:13:46 +0100

    in progress

--- src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/learningspringsecurity/demospringsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -61,7 +62,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/courses",true)
                 .and()
                 .rememberMe().tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                .key("somthingverysecured"); //defaults to 2 weeks;
+                .key("somthingverysecured")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login"); //defaults to 2 weeks;
     }
 
     @Override
